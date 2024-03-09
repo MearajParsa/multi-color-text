536ceb10ddccd16027306b3f4458cebe7d504b09
meerajparsa <meerajparsa@yahoo.com>
2024-03-09 17:37:52 +0330

    fixed rollup config error

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,8 +1,7 @@
-import resolve from "@rollup/plugin-node-resolve";
-import commonjs from "@rollup/plugin-commonjs";
-import typescript from "@rollup/plugin-typescript";
-import dts from "rollup-plugin-dts";
-import postcss from "rollup-plugin-postcss";
+import resolve from '@rollup/plugin-node-resolve'
+import commonjs from '@rollup/plugin-commonjs'
+import typescript from '@rollup/plugin-typescript'
+const dts = require('rollup-plugin-dts')
 
 const packageJson = require("./package.json");
 
@@ -24,14 +23,12 @@ export default [
     plugins: [
       resolve(),
       commonjs(),
-      typescript({ tsconfig: "./tsconfig.json" }),
-      postcss(),
+      typescript({ tsconfig: "./tsconfig.json" })
     ],
   },
   {
     input: "dist/esm/types/index.d.ts",
     output: [{ file: "dist/index.d.ts", format: "esm" }],
-    plugins: [dts()],
-    external: [/\.css$/],
+    plugins: [dts.default()]
   },
 ];
